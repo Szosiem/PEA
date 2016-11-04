051e2f7980bc25f2db85b6a3e68923f8e9915bff
szosiem <krykrystian@gmail.com>
2016-11-04 23:16:28 +0100

    Initial Commit

---
+++ PEA/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PEA
+//
+//  Created by Krystian Sliwinski on 2016-11-04.
+//  Copyright © 2016 Krystian Sliwinski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
